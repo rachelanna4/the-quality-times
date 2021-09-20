5ffce721d85dfce13c54dbcffda2657ff4f02f01
Rachel <rachelanna92@outlook.com>
2021-09-20 16:31:27 +0100

    add topics controller

--- routes/topics.router.js
+++ routes/topics.router.js
@@ -1,2 +1,5 @@
 const express = require("express");
 const topicsRouter = express.Router();
+const { fetchTopics } = require("../controllers/topics.controller.js");
+
+topicsRouter.get("/", getTopics);
