581e2350d3269267e52584a5ac7ea8c18542856a
Rachel <rachelanna92@outlook.com>
2021-09-20 12:49:11 +0100

    refactor into seperate createTables function with Promise.all removed

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,13 +1,10 @@
 const db = require("../connection.js");
 
-const seed = async (data) => {
-  const { articleData, commentData, topicData, userData } = data;
-
-  await Promise.all(
-    ["topics", "articles", "users", "comments"].map((tableName) => {
-      return db.query(`DROP TABLE IF EXISTS ${tableName};`);
-    })
-  );
+const createTables = async () => {
+  await db.query(`DROP TABLE IF EXISTS comments;`);
+  await db.query(`DROP TABLE IF EXISTS articles;`);
+  await db.query(`DROP TABLE IF EXISTS users;`);
+  await db.query(`DROP TABLE IF EXISTS topics;`);
 
   await db.query(
     `CREATE TABLE topics (
@@ -32,9 +29,9 @@ const seed = async (data) => {
       votes INT NOT NULL DEFAULT 0, 
       topic VARCHAR(20) NOT NULL,
       author VARCHAR(20) NOT NULL,
-      FOREIGN KEY (topic) REFERENCES topics(slug),
-      FOREIGN KEY (author) REFERENCES users(username),
-      created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+      created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+      FOREIGN KEY (topic) REFERENCES topics(slug) ON DELETE CASCADE,
+      FOREIGN KEY (author) REFERENCES users(username) ON DELETE CASCADE
       );`
   );
 
@@ -46,10 +43,16 @@ const seed = async (data) => {
       votes INT NOT NULL DEFAULT 0, 
       created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
       body VARCHAR(500) NOT NULL,
-      FOREIGN KEY (author) REFERENCES users(username),
-      FOREIGN KEY (article_id) REFERENCES articles(article_id)
+      FOREIGN KEY (author) REFERENCES users(username) ON DELETE CASCADE,
+      FOREIGN KEY (article_id) REFERENCES articles(article_id) ON DELETE CASCADE
       );`
   );
+};
+
+const seed = async (data) => {
+  const { articleData, commentData, topicData, userData } = data;
+
+  await createTables();
   // 1. create tables
   // 2. insert data
 };
