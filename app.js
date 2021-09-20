792aeae8d5ee71902edd713a19ee56d73085c358
Rachel <rachelanna92@outlook.com>
2021-09-20 16:27:21 +0100

    refactor apiRouter file path

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
-const apiRouter = require("../routes/api.router.js");
+const apiRouter = require("./routes/api.router.js");
 
 app.use("/api", apiRouter);
