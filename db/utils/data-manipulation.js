739365ea1ab9b7496e1a68df585ceb06daccc065
Rachel <rachelanna92@outlook.com>
2021-09-20 14:33:10 +0100

    refactor to fetchData function

--- db/utils/data-manipulation.js
+++ db/utils/data-manipulation.js
@@ -1,3 +1,7 @@
-exports.fetchTopics = (data) => {
+exports.fetchData = (data, ...args) => {
   if (data.length === 0) return [];
 };
+
+//   return data.map((topic) => {
+//     return [topic.slug, topic.desciption];
+//   });
