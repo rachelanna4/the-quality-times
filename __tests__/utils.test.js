06963ec47b0c9725d06ff3c3667e1180bbedd32e
Rachel <rachelanna92@outlook.com>
2021-09-20 14:36:55 +0100

    implement test to return nested array when passed an array with one object and the name of one key

--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -4,6 +4,11 @@ describe("fetchData", () => {
   test("returns an empty array when passed an empty array", () => {
     expect(fetchData([])).toEqual([]);
   });
+  test("when passed an array containing one object and the name of a key on that object, returns a two dimensional array containing the value of that key, ", () => {
+    const array = [{ description: "Code is love, code is life" }];
+    const key = "description";
+    expect(fetchData(array, key)).toEqual([["Code is love, code is life"]]);
+  });
 });
 
 // [
--- db/utils/data-manipulation.js
+++ db/utils/data-manipulation.js
@@ -1,5 +1,6 @@
 exports.fetchData = (data, ...args) => {
   if (data.length === 0) return [];
+  return [[data[0][args[0]]]];
 };
 
 //   return data.map((topic) => {
