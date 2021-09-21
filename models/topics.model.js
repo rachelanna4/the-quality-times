e5c1007e55dc2f302c60c48d95fe83ac22afcd71
Rachel <rachelanna92@outlook.com>
2021-09-21 12:04:54 +0100

    refactor to use async await

--- models/topics.model.js
+++ models/topics.model.js
@@ -1,7 +1,6 @@
 const db = require("../db/connection.js");
 
-exports.fetchTopics = () => {
-  return db.query(`SELECT * FROM topics`).then((result) => {
-    return result.rows;
-  });
+exports.fetchTopics = async () => {
+  const result = await db.query(`SELECT * FROM topics`);
+  return result.rows;
 };
