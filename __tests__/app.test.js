d16da7e5227fe2e23605b0fa43d0f541ce783a34
Rachel <rachelanna92@outlook.com>
2021-09-21 12:09:37 +0100

    refactor get api/topics test to include Array.isArray

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -13,7 +13,7 @@ describe("GET /api/topics", () => {
       .get("/api/topics")
       .expect(200)
       .then((res) => {
-        expect(res.body.topics).toEqual(expect.any(Array));
+        expect(Array.isArray(res.body.topics)).toEqual(true);
         expect(typeof res.body.topics[0]).toEqual("object");
       });
   });
