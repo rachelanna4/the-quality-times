e9e4e494529914380a036948bd2b01d5e2a66c99
Rachel <rachelanna92@outlook.com>
2021-09-21 12:19:21 +0100

    refactor app.test to use async await

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -8,13 +8,16 @@ beforeEach(() => seed(testData));
 afterAll(() => db.end());
 
 describe("GET /api/topics", () => {
-  test("200: returns an array of objects", () => {
-    return request(app)
-      .get("/api/topics")
-      .expect(200)
-      .then((res) => {
-        expect(Array.isArray(res.body.topics)).toEqual(true);
-        expect(typeof res.body.topics[0]).toEqual("object");
-      });
+  test("200: returns an array of objects", async () => {
+    const res = await request(app).get("/api/topics").expect(200);
+    expect(Array.isArray(res.body.topics)).toEqual(true);
+    expect(typeof res.body.topics[0]).toEqual("object");
   });
 });
+// test("200: returns an array of correct length", () => {
+//   return request(app)
+// });
+// });
+
+// 200: objects returned in the array have the correct keys
+// test("200: returns an array containing the expected topics")
