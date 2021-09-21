97933d704e557a8b847d393dc8900d7253bb4d07
Rachel <rachelanna92@outlook.com>
2021-09-21 16:00:27 +0100

    refactor handle400errors to use err.status and err.msg

--- errors.js
+++ errors.js
@@ -1,6 +1,6 @@
 exports.handle400errors = (err, req, res, next) => {
   if (err.status === 400) {
-    res.status(400).send({ msg: "Bad request" });
+    res.status(err.status).send({ msg: err.msg });
   }
 };
 
