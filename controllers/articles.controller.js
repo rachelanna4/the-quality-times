47bc6910915484b195dbfc2ea8ffa8ea4867e9b9
Rachel <rachelanna92@outlook.com>
2021-09-22 10:11:13 +0100

    irefactor controller function for /api/articles/:article_id to use fetchArticleById function

--- controllers/articles.controller.js
+++ controllers/articles.controller.js
@@ -17,7 +17,9 @@ exports.patchArticleById = async (req, res, next) => {
   try {
     const { article_id } = req.params;
     const { inc_votes } = req.body;
-    const patchedArticle = await updateArticleById(article_id, inc_votes);
+    await fetchArticleById(article_id);
+    await updateArticleById(article_id, inc_votes);
+    const patchedArticle = await fetchArticleById(article_id);
     res.status(200).send(patchedArticle);
   } catch (err) {
     next(err);
