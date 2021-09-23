08b41702454412e3ebee506e1689512e448879a1
Rachel <rachelanna92@outlook.com>
2021-09-23 11:55:04 +0100

    refactor app.listen function

--- listen.js
+++ listen.js
@@ -2,6 +2,6 @@ const app = require("./app.js");
 
 const PORT = process.env.PORT || 9090;
 
-app.listen((PORT) => {
-  console.log("app is listening on port ${PORT}...");
+app.listen(PORT, () => {
+  console.log(`app is listening on port ${PORT}...`);
 });
