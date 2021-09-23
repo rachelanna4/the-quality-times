05f29bc90bb924c35b8a4c6817d160678d2dacce
Rachel <rachelanna92@outlook.com>
2021-09-23 15:09:55 +0100

    add assertion to  get /api test to check returned json object contains all available endpoints

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -420,6 +420,19 @@ describe("GET /api", () => {
   });
   test("200: returned object contains all available endpoints", async () => {
     const res = await request(app).get("/api").expect(200);
-    expect(Object.keys(res.body).length).toBe(7);
+    const returnedEndpoints = Object.keys(res.body);
+    const availableEndpoints = [
+      "GET /api",
+      "GET /api/topics",
+      "GET /api/articles",
+      "GET /api/articles/:article_id",
+      "PATCH /api/articles/:article_id",
+      "GET /api/articles/:article_id/comments",
+      "POST /api/articles/:article_id/comments",
+    ];
+    expect(returnedEndpoints.length).toBe(7);
+    availableEndpoints.forEach((endpoint) => {
+      expect(returnedEndpoints.includes(endpoint)).toBe(true);
+    });
   });
 });
