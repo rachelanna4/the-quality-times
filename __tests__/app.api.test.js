9822630b2f00cc78aac3ed4e4fcc7f210baa35b1
Rachel <rachelanna92@outlook.com>
2021-09-27 11:48:28 +0100

    add all endpoints to get/api

--- __tests__/app.api.test.js
+++ __tests__/app.api.test.js
@@ -19,13 +19,20 @@ describe("GET /api", () => {
     const availableEndpoints = [
       "GET /api",
       "GET /api/topics",
+      "POST /api/topics",
       "GET /api/articles",
+      "POST /api/articles",
       "GET /api/articles/:article_id",
       "PATCH /api/articles/:article_id",
+      "DELETE /api/articles/:article_id",
       "GET /api/articles/:article_id/comments",
       "POST /api/articles/:article_id/comments",
+      "PATCH /api/comments/:comment_id",
+      "DELETE /api/comments/:comment_id",
+      "GET /api/users",
+      "GET /api/users/:username",
     ];
-    expect(returnedEndpoints.length).toBe(7);
+    expect(returnedEndpoints.length).toBe(14);
     availableEndpoints.forEach((endpoint) => {
       expect(returnedEndpoints.includes(endpoint)).toBe(true);
     });
